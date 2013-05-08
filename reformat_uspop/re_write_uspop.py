5fad654e8bfd4d49e73969ac5b22c08a13336a79
mattmcvicar <mattmcvicar@hotmail.com>
2013-05-08 11:58:40 -0400

    Added server_names.txt to copy mp3s to local directory

--- reformat_uspop/re_write_uspop.py
+++ reformat_uspop/re_write_uspop.py
@@ -2,10 +2,13 @@
 
 import os, re
 
-os.chdir('/Users/mattmcvicar/Desktop/Work/New_chroma_features/Package/USpoplabs/')
+os.chdir('/Users/mattmcvicar/Desktop/Work/New_chroma_features/Package/reformat_uspop/')
 
 root_dir = '/Users/mattmcvicar/Desktop/Work/New_chroma_features/Package/USpoplabs/uspopLabels/'
 new_dir = '/Users/mattmcvicar/Desktop/Work/New_chroma_features/Package/USpoplabs_flat/'
+
+server_root = '/u/drspeech/data/music/mp3s/'
+server_names = open('./server_names.txt','w+')
 for path, subdirs, files in os.walk(root_dir):
     for name in files:
         if os.path.splitext(name)[1] == '.lab':
@@ -15,3 +18,9 @@ for path, subdirs, files in os.walk(root_dir):
           # Trim number from name
           name_nonum = name[3:]
           os.system('cp ' + os.path.join(path, name) + ' ' + new_dir + artist + '_-_' +  name_nonum)
+          
+          # Also get the name on the server
+          server_name = server_root + '/'.join(re.split('/',path)[-2:]) + '/' + name[:-len('lab')] + 'mp3'
+          server_names.write(server_name + '\n')
+          
+server_names.close()          
