e9e6f8158539fe9f52c42a4727cb9060d0df8329
Brian McFee <brm2132@columbia.edu>
2013-05-12 12:14:35 -0400

    revised cdl_compress to support fda as well

--- cdl_compress.py
+++ cdl_compress.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-"""CDL encoding PCA fitter
+"""CDL encoding transformer fitter
 CREATED:2013-05-08 16:15:55 by Brian McFee <brm2132@columbia.edu>
 
 Usage:
 
 ./cdl_compress.py n_jobs pca_model.pickle /path/to/octarines/glob
 
-Once we have a PCA model and a set of -encoded.npy files, use PCA to compress them.
+Once we have a transformer model and a set of -encoded.npy files, use transformer to compress them.
 
 Saves output alongside as -encoded-compressed.npy
 
@@ -25,7 +25,7 @@ RETAIN = 0.95
 def vectorize(A):
     return A.squeeze().reshape((A.shape[0], -1))
 
-def process_song(PCA, d, song):
+def process_song(transformer, d, song, ext):
 
     songname = os.path.basename(song)
     songname = songname[:songname.index('-encoded.npy')]
@@ -34,25 +34,31 @@ def process_song(PCA, d, song):
     A = vectorize(np.load(song))
 
     # Transform the data, project to top $RETAIN variance dimensions
-    Ahat = PCA.transform(A)[:,:d]
+    Ahat = transformer.transform(A)[:,:d]
 
-    outname = '%s/%s-encoded-compressed.npy' % (os.path.dirname(song), songname)
+    outname = '%s/%s-encoded-%s.npy' % (os.path.dirname(song), songname, ext)
     np.save(outname, Ahat)
     pass
 
-def process_data(n_jobs, PCA, file_glob):
+def process_data(n_jobs, transformer, file_glob, ext):
     files = glob.glob(file_glob)
     files.sort()
 
-    d = np.argmax(np.cumsum(PCA.explained_variance_ratio_) >= RETAIN)
+    d = -1
+    if hasattr(transformer, 'explained_variance_ratio_'):
+        d = np.argmax(np.cumsum(transformer.explained_variance_ratio_) >= RETAIN)
 
-    Parallel(n_jobs=n_jobs)(delayed(process_song)(PCA, d, song) for song in files)
+    Parallel(n_jobs=n_jobs)(delayed(process_song)(transformer, d, song, ext) for song in files)
     pass
 
 if __name__ == '__main__':
     n_jobs = int(sys.argv[1])
     with open(sys.argv[2], 'r') as f:
-        PCA = pickle.load(f)
+        transformer = pickle.load(f)
 
     file_glob = sys.argv[3]
-    process_data(n_jobs, PCA, file_glob)
+    if len(sys.argv) > 4:
+        ext = sys.argv[4]
+    else:
+        ext = 'compressed'
+    process_data(n_jobs, transformer, file_glob, ext)
